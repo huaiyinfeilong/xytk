f58ef3067c2286cb47a6b445c34bdcea46ce2849
huaiyinfeilong <huaiyinfeilong@163.com>
2019-12-05 18:21:40 +0800

    '更新代码。'

---
+++ myapp/lib/channel.dart
@@ -0,0 +1,10 @@
+﻿import "package:flutter/material.dart";
+
+class Channel extends StatelessWidget {
+	@override
+	Widget build(BuildContext context) {
+		return Material(
+			color: Colors.yellow
+		);
+	}
+}
---
+++ myapp/lib/me.dart
@@ -0,0 +1,10 @@
+﻿import "package:flutter/material.dart";
+
+class Me extends StatelessWidget {
+	@override
+	Widget build(BuildContext context) {
+		return Material(
+			color: Colors.blue
+		);
+	}
+}
